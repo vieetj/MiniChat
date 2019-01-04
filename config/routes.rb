1160e5c9b896ca00fdb2d3ae48a348915e64efb3
vieetj <lethanhviet.bay@gmail.com>
2019-01-04 17:46:09 +0700

    Create controller ChatRoomsController and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'chat_rooms/index'
+  get 'chat_rooms/new'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :chat_rooms
 end
