3ad0fca8f6a2f9c2bcc89544b22b8e6807285be4
vieetj <lethanhviet.bay@gmail.com>
2019-01-04 17:36:46 +0700

    Create restrict access, generate model ChatRoom, define association between ChatRoom and User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_04_103221) do
+ActiveRecord::Schema.define(version: 2019_01_04_103431) do
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_chat_rooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
